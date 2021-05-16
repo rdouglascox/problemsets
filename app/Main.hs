60abd865ed0f2caeebd19941a9bb98374d11f5ca
Ryan Cox <ryan@rdouglascox.com>
2021-05-16 21:17:26 +1000

    more optparse

--- app/Main.hs
+++ app/Main.hs
@@ -36,7 +36,14 @@ basic1 = do
 batch :: Int -> IO ()
 batch n =  replicateConcurrently_ n basic
 
+basic2 :: Int -> IO ()
+basic2 num = do
+       let seed = mkStdGen num
+       mapConcurrently_ id [mkps07g seed num, mkps08g seed num, mkps09g seed num, mkps10g seed num]
+       return()
 
+batch2 :: Int -> Int -> IO ()
+batch2 n1 n2 =  replicateConcurrently_ n1 (basic2 n2)
 
 main :: IO ()
 main = mode =<< execParser opts
@@ -47,13 +54,14 @@ main = mode =<< execParser opts
      <> header "problemsets - generate problem sets for logic" )
 
 mode :: MyOptions -> IO ()
-mode (x,_,_,_) = batch x
+mode opts | ident(opts) /= 0 = batch2 (bsize(opts)) (ident(opts))
+          | otherwise =  batch (bsize(opts))
 
 data MyOptions = MyOptions
   { bsize      :: Int
   , psets      :: [Int]
   , ident      :: Int
-  , file       :: Int
+  , file       :: String
   }
 
 sample :: Parser MyOptions
@@ -61,29 +69,29 @@ sample = MyOptions
       <$> option auto
           ( long "batch-size"
          <> short 'b'
-         <> help "Batch size."
+         <> help "Batch size"
          <> showDefault
          <> value 1
          <> metavar "INT" )
       <*> option auto
           ( long "problem-sets"
          <> short 'p'
-         <> help "Which problem sets."
+         <> help "Which problem sets"
          <> showDefault
-         <> value 1
+         <> value [1]
          <> metavar "[INT]" )
       <*> option auto
           ( long "identifier"
          <> short 'i'
-         <> help "Problem set identifier."
+         <> help "Problem set identifier"
          <> showDefault
-         <> value 1
+         <> value 0
          <> metavar "INT" )
-      <*> option auto
-          ( long "enthusiasm"
+      <*> strOption
+          ( long "file"
          <> short 'f'
-         <> help "Identifier file."
+         <> help "Identifier file"
          <> showDefault
-         <> value 1
-         <> metavar "INT" )
+         <> value "identifiers.txt"
+         <> metavar "FILENAME" )
 
