b4e7dfebbfe3531be89f4826b72add638361bb95
Ryan Cox <ryan@rdouglascox.com>
2021-05-16 14:56:59 +1000

    batching

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Control.Concurrent.Async ( async, wait, (mkps07g seed num) )
+import Control.Concurrent.Async ( async, wait,  mapConcurrently_, replicateConcurrently_ )
 import Control.Monad
 import System.Environment
 
@@ -14,17 +14,25 @@ import MakePS.MakePS10 ( mkps10g )
 
 import System.Random ( newStdGen, next, mkStdGen )
 
-main =
-       batch 4
+main = do
+       arg <- getArgs
+       batch (read (head (arg)) :: Int)
 
 basic :: IO ()
 basic = do
        g <- newStdGen    -- get random generator
        let (num,_) = next g  -- use it to get a random number
        let seed = mkStdGen num
+       mapConcurrently_ id [mkps07g seed num, mkps08g seed num, mkps09g seed num, mkps10g seed num]
+       return()
+
+basic1 :: IO ()
+basic1 = do
+       g <- newStdGen    -- get random generator
+       let (num,_) = next g  -- use it to get a random number
+       let seed = mkStdGen num
        mapConcurrently_ id [mkps01,mkps02,mkps04, mkps07g seed num, mkps08g seed num, mkps09g seed num, mkps10g seed num]
        return()
 
-
 batch :: Int -> IO ()
-batch n = replicateM_ n main
+batch n =  replicateConcurrently_ n basic
